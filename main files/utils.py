8c427623eaac4a8a2fe87435066f54e12e099958
agunjal00 <agunjal@stevens.edu>
2026-05-06 21:26:25 -0400

    Update utils.py

--- main files/utils.py
+++ main files/utils.py
@@ -6,3 +6,9 @@ def convert_steps_to_miles(steps_list):
 
     # Convert each step count into miles and round down
     return list(map(lambda s: math.floor(s * 0.0005), steps_list))
+    
+def filter_high_heart_rate(entries, threshold):
+    return list(filter(lambda e: e.heart_rate > threshold, entries))
+
+
+def get_step_counts(entries):
